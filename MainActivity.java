f17604531174b1dade8f097dc77c35ede9ffaedd
Gabriel Gomi <113320332+yKenj1@users.noreply.github.com>
2022-10-24 14:48:31 -0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,121 +1,34 @@
-package com.example.imagedice;
+package br.com.bq.multitask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
-import android.widget.TextView;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void openActivity(View view, Class myClass){
+
+        Intent intent = new Intent(this, myClass);
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonListenerMethod();
-        seekBarSize();
-    }
-    public void  seekBarSize(){
-        final ImageView diceSize = findViewById(R.id.DiceImage);
-        SeekBar BarSize = findViewById(R.id.seekBarSize);
-        BarSize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        Button btnChange2 = findViewById(R.id.btnChange2);
+        btnChange2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                switch (progress){
-                    case 1:
-                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(10, 10);
-                        diceSize.setLayoutParams(layoutParams);
-                        break;
-
-                    case 2:
-                        layoutParams = new LinearLayout.LayoutParams(20, 20);
-                        diceSize.setLayoutParams(layoutParams);
-                        break;
-
-                    case 3:
-                        layoutParams = new LinearLayout.LayoutParams(30,30);
-                        diceSize.setLayoutParams(layoutParams);
-                        break;
-
-                    case 4:
-                        layoutParams = new LinearLayout.LayoutParams(40,40);
-                        diceSize.setLayoutParams(layoutParams);
-                        break;
-
-                    case 5:
-                        layoutParams = new LinearLayout.LayoutParams(50,50);
-                        diceSize.setLayoutParams(layoutParams);
-                        break;
-
-                }
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                diceSize.setVisibility(View.VISIBLE);
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                diceSize.setVisibility(View.INVISIBLE);
-
+            public void onClick(View view) {
+                openActivity(view, Main2Activity.class);
             }
         });
 
-    }
-
-    public void buttonListenerMethod(){
-
-        Button btn = findViewById(R.id.buttonRoll);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Random rnd = new Random();
-                int resultRnd = rnd.nextInt(6) + 1;
-                TextView returnText = findViewById(R.id.DiceResult);
-                returnText.setText("Resultado do dado: " + Integer.toString(resultRnd));
-                ImageView diceImage = findViewById(R.id.DiceImage);
-                switch (resultRnd){
-                    case 1:
-                        diceImage.setImageResource(R.drawable.f1);
-                        break;
-
-                    case 2:
-                        diceImage.setImageResource(R.drawable.f2);
-                        break;
-
-                    case 3:
-                        diceImage.setImageResource(R.drawable.f3);
-                        break;
-
-                    case 4:
-                        diceImage.setImageResource(R.drawable.f4);
-                        break;
-
-                    case 5:
-                        diceImage.setImageResource(R.drawable.f5);
-                        break;
-
-                    case 6:
-                        diceImage.setImageResource(R.drawable.f6);
-                        break;
-
-                }
-
-            }
-        });
 
     }
-
 }
