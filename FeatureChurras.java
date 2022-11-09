924aa6e07913dabd9da650cfdb8e386202d978bb
Gabriel Gomi <113320332+yKenj1@users.noreply.github.com>
2022-11-09 12:46:01 -0300

    Add files via upload

--- FeatureChurras.java
+++ FeatureChurras.java
@@ -1,9 +1,11 @@
 package com.example.tcc_prototype;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class FeatureChurras extends AppCompatActivity {
 
@@ -12,13 +14,32 @@ public class FeatureChurras extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feature_screen);
 
+        public void response(){
+
+
+
+        }
+
         Button btnReserva = findViewById(R.id.btnReserve);
-
-
         btnReserva.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                public void onCheckboxClicked(View view){
+
+                    boolean check = ((CheckBox) view).isChecked();
+
+                    switch(view.getId()){
+                        case R.id.checkBox:
+                            if (check){
+                                AlertDialog.Builder popUp = new AlertDialog.Builder();
+                                popUp.setMessage();
+
+                            }else{
+
+                            }
+                    }
+                }
             }
         });
 
