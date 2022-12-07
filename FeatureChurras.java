162132ca7b96e01cb4b7d6001ff478d0e4628a29
Gabriel Gomi <113320332+yKenj1@users.noreply.github.com>
2022-12-07 08:38:25 -0300

    Add files via upload

--- FeatureChurras.java
+++ FeatureChurras.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class FeatureChurras extends AppCompatActivity {
 
@@ -13,12 +14,6 @@ public class FeatureChurras extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feature_screen);
-
-        public void response(){
-
-
-
-        }
 
         Button btnReserva = findViewById(R.id.btnReserve);
         btnReserva.setOnClickListener(new View.OnClickListener() {
@@ -32,14 +27,70 @@ public class FeatureChurras extends AppCompatActivity {
                     switch(view.getId()){
                         case R.id.checkBox:
                             if (check){
-                                AlertDialog.Builder popUp = new AlertDialog.Builder();
-                                popUp.setMessage();
+                                AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+                                popUp.setMessage("Reserva concluída com sucesso");
+                                popUp.setNeutralButton("Ok", null);
+                                popUp.show();
 
                             }else{
+                                AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+                                popUp.setMessage("Reserva retirada com sucesso");
+                                popUp.setNeutralButton("Ok", null);
+                                popUp.show();
+
+                            }
+                        case R.id.checkBox2:
+                            if (check){
+                                AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+                                popUp.setMessage("Reserva concluída com sucesso");
+                                popUp.setNeutralButton("Ok", null);
+                                popUp.show();
+
+                            }else{
+                                AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+                                popUp.setMessage("Reserva retirada com sucesso");
+                                popUp.setNeutralButton("Ok", null);
+                                popUp.show();
+
+                            }
+                        case R.id.checkBox3:
+                            if (check){
+                                AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+                                popUp.setMessage("Reserva concluída com sucesso");
+                                popUp.setNeutralButton("Ok", null);
+                                popUp.show();
+
+                            }else{
+                                AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+                                popUp.setMessage("Reserva retirada com sucesso");
+                                popUp.setNeutralButton("Ok", null);
+                                popUp.show();
+
+                            }
+                        case R.id.checkBox4:
+                            if (check){
+                                AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+                                popUp.setMessage("Reserva concluída com sucesso");
+                                popUp.setNeutralButton("Ok", null);
+                                popUp.show();
+
+                            }else{
+                                AlertDialog.Builder popUp = new AlertDialog.Builder(this);
+                                popUp.setMessage("Reserva retirada com sucesso");
+                                popUp.setNeutralButton("Ok", null);
+                                popUp.show();
 
                             }
                     }
-                }
+                };
+            }
+        });
+
+        Button btnFinish = findViewById(R.id.btnFinish);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
 
